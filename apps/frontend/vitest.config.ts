809f7a0a4c522930e1649094014d0076c932a03a
pehtoorismies <pehtorismies@gmail.com>
2025-11-29 11:25:41 +0200

    chore(frontend): add mantine, tests

---
+++ apps/frontend/vitest.config.ts
@@ -0,0 +1,13 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    exclude: ['build', 'node_modules', 'tmp'],
+    globals: true,
+    reporters: ['verbose'],
+    setupFiles: ['./vitest.setup.ts'],
+  },
+})
