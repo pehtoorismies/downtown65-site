eaeea16dd08e91f58c7663ad8debcd5146a9f7f3
pehtoorismies <pehtorismies@gmail.com>
2026-01-08 13:45:04 +0200

    ci: d1 migrations

--- apps/backend/drizzle_flat/20251225110331_rename_event_creator.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `events` RENAME COLUMN `createdBy` TO `creatorId`;
--- apps/backend/scripts/drizzle_flatten.sh
+++ apps/backend/scripts/drizzle_flatten.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+mkdir -p ./drizzle_flat
 find ./drizzle -name migration.sql -print0 | xargs -0 -I{} bash -c 'cp "{}" "./drizzle_flat/$(basename $(dirname {})).sql"'
