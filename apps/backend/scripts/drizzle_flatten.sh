5f7e2f14384a5871f9797d83b4a2bf1fd1234ebb
pehtoorismies <pehtorismies@gmail.com>
2026-01-13 10:57:02 +0200

    chore: create local dev environment

--- apps/backend/scripts/drizzle_flatten.sh
+++ apps/backend/scripts/drizzle_flatten.sh
@@ -2,3 +2,5 @@
 
 mkdir -p ./drizzle_flat
 find ./drizzle -name migration.sql -print0 | xargs -0 -I{} bash -c 'cp "{}" "./drizzle_flat/$(basename $(dirname {})).sql"'
+
+echo "Flattened migrations copied to ./drizzle_flat"
