307a22d98621c6f5543a7ee068ee72acf82cc745
pehtoorismies <pehtorismies@gmail.com>
2026-01-18 21:02:35 +0200

    fix: use url instead of host

--- apps/frontend/app/api/api-client.ts
+++ apps/frontend/app/api/api-client.ts
@@ -2,5 +2,5 @@ import createClient from 'openapi-fetch'
 import type { paths } from './+types/api.d'
 
 export const getApiClient = (apiHost: string) => {
-  return createClient<paths>({ baseUrl: apiHost })
+  return createClient<paths>({ baseUrl: `https://${apiHost}` })
 }
