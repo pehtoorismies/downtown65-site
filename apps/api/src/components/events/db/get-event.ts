c7b2f845c1dd5605477a3a2b26840be88ec6c5ef
pehtoorismies <pehtorismies@gmail.com>
2026-02-13 10:29:22 +0200

    feat(api): order participants

--- apps/api/src/components/events/db/get-event.ts
+++ apps/api/src/components/events/db/get-event.ts
@@ -1,8 +1,10 @@
 import type { Event, ID, ULID } from '@downtown65/schema'
 import { EventSchema } from '@downtown65/schema'
+import { eq, sql } from 'drizzle-orm'
 
 import type { RequestContext } from '~/app-api'
 import { getDb } from '~/db/get-db'
+import { users, usersToEvent } from '~/db/schema'
 
 export const getEvent = async (
   ctx: RequestContext,
@@ -18,7 +20,6 @@ export const getEvent = async (
     },
     with: {
       createdBy: true,
-      participants: true,
     },
   })
 
@@ -28,15 +29,23 @@ export const getEvent = async (
     return undefined
   }
 
-  const e = {
-    ...event,
-    participants: event.participants.map((p) => {
-      return {
-        ...p,
-        joinedAt: new Date().toISOString(),
-      }
-    }),
-  }
+  // HACK - Drizzle doesn't support joinedAt in query
+  const participantRows = await db
+    .select({
+      auth0Sub: users.auth0Sub,
+      id: users.id,
+      // SQLite's `CURRENT_TIMESTAMP` format (`2025-01-15 14:30:00`)
+      // but  Participant expects ISO format (`2025-01-15T14:30:00Z`), so we replace the space with 'T' and append 'Z'
+      joinedAt: sql<string>`replace(${usersToEvent.createdAt}, ' ', 'T') || 'Z'`,
+      nickname: users.nickname,
+      picture: users.picture,
+    })
+    .from(usersToEvent)
+    .innerJoin(users, eq(usersToEvent.userId, users.id))
+    .where(eq(usersToEvent.eventId, event.id))
 
-  return EventSchema.decode(e)
+  return EventSchema.decode({
+    ...event,
+    participants: participantRows,
+  })
 }
